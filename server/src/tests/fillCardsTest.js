1b1575f75b1b13c360d6f2a33d596a4777c1e197
esteban <estebangagneur03@gmail.com>
2025-02-17 22:09:10 +0100

    [BUGFIX] test préremplissage des cartes manuel

--- server/src/tests/fillCardsTest.js
+++ server/src/tests/fillCardsTest.js
@@ -9,42 +9,45 @@ export default class fillCardsTest {
 
     test() {
         this.game.NUMBER_OF_CARDS_PER_FAMILY = 15
-        this.game.eventDispatcher.on('changeState', (state) => {
-            console.log(state)
-            // if (state === STATE.PLAYING) {
-            //     for (let i = 0; i < this.turns; i++) {
-            //
-            //         if (this.game.cards < 1) {
-            //             break
-            //         }
-            //
-            //         for (const user of this.game.users) {
-            //             if (user.socket.id === this.game.userTurnId) {
-            //
-            //                 this.game.eventDispatcher.emit('client/play', {
-            //                     action: Math.random() > .5 ? 'enlight' : 'shadow',
-            //                     familyId: user.handCards[0].family.id,
-            //                     cardId: user.handCards[0].id,
-            //                 }, user.socket)
-            //
-            //                 this.game.eventDispatcher.emit('client/play', {
-            //                     action: 'give',
-            //                     familyId: user.handCards[0].family.id,
-            //                     cardId: user.handCards[0].id,
-            //                     toUserId: this.game.users.find(u => u.socket.id !== user.socket.id).socket.id,
-            //                 }, user.socket)
-            //
-            //                 this.game.eventDispatcher.emit('client/play', {
-            //                     action: 'keep',
-            //                     familyId: user.handCards[0].family.id,
-            //                     cardId: user.handCards[0].id,
-            //                 }, user.socket)
-            //             }
-            //         }
-            //     }
-            // }
 
-        })
+        if (this.game.state === STATE.PLAYING) {
+            for (let i = 0; i < this.turns; i++) {
+
+                if (this.game.cards < 1) {
+                    break
+                }
+
+                for (const user of this.game.users) {
+                    if (user.socket.id === this.game.userTurnId) {
+
+                        this.game.eventDispatcher.emit('client/play', {
+                            action: Math.random() > .5 ? 'enlight' : 'shadow',
+                            familyId: user.handCards[0].family.id,
+                            cardId: user.handCards[0].id,
+                        }, user.socket)
+
+                        this.game.eventDispatcher.emit('client/play', {
+                            action: 'give',
+                            familyId: user.handCards[0].family.id,
+                            cardId: user.handCards[0].id,
+                            toUserId: this.game.users.find(u => u.socket.id !== user.socket.id).socket.id,
+                        }, user.socket)
+
+                        this.game.eventDispatcher.emit('client/play', {
+                            action: 'keep',
+                            familyId: user.handCards[0].family.id,
+                            cardId: user.handCards[0].id,
+                        }, user.socket)
+                    }
+                }
+            }
+        }
+
+        // this.game.eventDispatcher.on('changeState', (state) => {
+        //     console.log(state)
+        //
+        //
+        // })
     }
 
 }
